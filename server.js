2b923404fe4ffa13f0e7c16807a332ce394d1214
rick <technoweenie@gmail.com>
2010-04-01 20:06:45 -0700

    add basic pinging

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var sys = require('sys'),
-     ws = require('ws')
+     ws = require('ws'),
      tr = require('./lib/tracker')
 
 var tracker = tr.create()
 var server  = ws.createServer(function (socket) {
+  var record
   socket.addListener("connect", function (res) {
-    tracker.connect(socket, res)
+    record = tracker.connect(socket, res)
+    sys.puts('connected: ' + socketId)
   })
   socket.addListener("data", function (data) {
     sys.puts('> ' + sys.inspect(data))
   })
-  socket.addListener("close", function (res) {
-    tracker.disconnect(socket)
+  socket.addListener("close", function () {
+    sys.puts('disconnected: ' + record.id)
+    tracker.disconnect(record)
   })
 })
 server.listen(3840)
+
+GLOBAL.tracker = tracker
+
+require("repl").start("> ")
